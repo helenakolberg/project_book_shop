47959fc77683c74d77041979e08a776f70af0d37
Helena <inger.helena@gmail.com>
2020-07-13 15:41:16 +0100

    added create route

--- controllers/authors_controller.rb
+++ controllers/authors_controller.rb
@@ -8,3 +8,13 @@ get '/authors' do
     @authors = Author.all()
     erb(:"authors/index")
 end
+
+get '/authors/new' do
+    @authors = Author.all()
+    erb(:"authors/new")
+end
+
+post '/authors' do
+    Author.new(params).save()
+    redirect '/authors'
+end
